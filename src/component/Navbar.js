6ac755c799b97e788401b56e28a219df09a621bc
BISWAJIT GHOSH <95334156+Biswajit721159@users.noreply.github.com>
2022-09-18 19:27:40 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -35,15 +35,6 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
           <div className="container ">
             <button
               onClick={toggelStyle}
