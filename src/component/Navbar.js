3938b2174e2b7a50f5a98e7a01d356ccc84fde37
BISWAJIT GHOSH <95334156+Biswajit721159@users.noreply.github.com>
2022-10-02 12:23:51 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,11 +8,11 @@ export default function Navbar() {
   const toggelStyle = () => {
     if (mode === "light") {
       setmode("dark");
-      document.body.style.backgroundColor = "#3D3D3B";
+      document.body.style.backgroundColor = "#282525";
       setbtnText("Light mode on");
     } else if (mode === "dark") {
       setmode("light");
-      document.body.style.backgroundColor = "rgb(255,255,255)";
+      document.body.style.backgroundColor = "#F4F6F6";
       setbtnText("Dark mode on");
     }
   };
@@ -24,17 +24,6 @@ export default function Navbar() {
           <Link className="navbar-brand" to="/">
             Home
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="container ">
             <button
               onClick={toggelStyle}
