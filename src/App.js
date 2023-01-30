a9a30208e5309e02a688ee1230c7a2e0a177d08c
BISWAJIT GHOSH <95334156+Biswajit721159@users.noreply.github.com>
2023-01-30 11:05:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
+// import "./App.css";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 
-import Main from "./other/Main";
-import Error from "./other/Error";
-import Adduser from "./other/Adduser";
-import Navbar from "./other/Navbar";
-import Updateuser from "./other/Updateuser";
+// import Main from "./other/Main";
+// import Error from "./other/Error";
+// import Adduser from "./other/Adduser";
+// import Navbar from "./other/Navbar";
+// import Updateuser from "./other/Updateuser";
 
 
+
+// function App() {
+ 
+//   return (
+//     <>
+//       <Router>
+//         <Navbar/>
+//          <Routes>
+//            <Route path="/" element={ <Main/>}></Route>
+//            <Route path="/adduser" element={ <Adduser/>}></Route>
+//            <Route path="/user/:id/edit" element={ <Updateuser/>}></Route>
+//            <Route path="*" element={<Error/>}></Route>
+//          </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+
+
+import Navbar from "./component/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ProductFrom } from "./component/ProductFroms";
+import Error  from "./component/Error";
+import Adduser from "./component/Adduser";
 
 function App() {
  
@@ -26,5 +52,6 @@ function App() {
     </>
   );
 }
+
 
 export default App;
